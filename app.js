26b7e3c36f68dbc257b9126625edfcba2b3ff84f
Hakis1 <t1hajo02@students.oamk.fi>
2022-02-26 19:43:15 +0200

    teht. 17 tehty

--- app.js
+++ app.js
@@ -2,20 +2,28 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const bookRouter = require('./routes/book');
+const borrowerRouter = require('./routes/borrower');
+const userRouter = require('./routes/user');
+const basicAuth = require('express-basic-auth');
 
 var app = express();
 
-//Heihei!!
+app.use(basicAuth({users: { 'admin': '1234' }}))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet());
+app.use(cors());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/book', bookRouter);
+app.use('/borrower', borrowerRouter);
+app.use('/user', userRouter);
+
 
 module.exports = app;
